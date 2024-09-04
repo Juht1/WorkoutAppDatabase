e0320c1be44c9892f6119f16a50436352a2300d0
Juht1 <kristjan.juht@gmail.com>
2024-09-04 14:14:53 +0300

    Added bcrypt

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+
+const users = []
 
 app.set("view engine", "ejs")
-
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
-    res.render('index')
+    res.render('index.ejs')
 })
+
+app.get("/login", (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.get("/register", (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+    req.body.name
+  });
+  
 
 
 app.listen(3000)
